953f6ed4fd44b5625aa70acfe5143a2eb9f6bfce
Özgür KAN <galatasaray_ozgur_94@hotmail.com>
2021-02-16 22:20:54 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,23 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+import math
+import random
+
+def isPrime(n):
+    for i in range(2, int(math.sqrt(n)+1)):
+        if n % i == 0: 
+            return False;
+    return n>1;
+primeList=[2]
+for n in range(3, 1000):
+    if isPrime(n):
+        primeList.append(n)
+
+matris = [[0, 0, 0], [0, 0, 0],[0, 0, 0]]
+for i in range(3):
+  for j in range(3):
+    tutulanSayi=random.randint(0,len(primeList)-1)
+    matris[i][j]=primeList[tutulanSayi]
+    primeList.pop(tutulanSayi)
+print(matris)
